6f390d3a6b03c99736578b1c4273be5d3dd98966
sinthiyakalimuthu <sinthiya.kalimuthu@syncfusion.com>
2023-03-10 15:55:21 +0530

    bug(BLAZ-29505): RTL sample with locale

--- RTL/RTL Sample/Program.cs
+++ RTL/RTL Sample/Program.cs
@@ -1,32 +1,49 @@
+using RTL_Sample.Data;
+using RTL_Sample.Shared;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using RTL_Sample.Data;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 using Syncfusion.Blazor;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor().AddHubOptions(o => { o.MaximumReceiveMessageSize = 102400000; });
-// Add Syncfusion Blazor service to the container.
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddControllers();
+
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+//Register the Syncfusion locale service to localize Syncfusion Blazor components.
+builder.Services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SyncfusionLocalizer));
 builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("OTQ5NTMyQDMyMzAyZTM0MmUzMENJSkhyTDZjUjFPd1lyclhPRi9wSGZlazFiZlE1TnNPcCtQNlEyYUJ2cW89");
+
+
+var supportedCultures = new[] { "en-US", "de", "fr", "ar", "sv" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[3])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
 
 var app = builder.Build();
-
+app.UseRequestLocalization(localizationOptions);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
