26297b87a39639c86c4161cdf8d625a706e379af
SaravanaPriya31 <saravanapriya.s@syncfusion.com>
2023-06-13 16:22:11 +0530

    commit

---
+++ Toolbar/Annotation Toolbar/Show or hide on loading - SfPdfViewer/Show_or_hide_on_loading/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Show_or_hide_on_loading.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ Toolbar/Custom Toolbar/Custom toolbar with PNG image - SfPdfViewer/Custom_toolbar_with_PNG_image/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Custom_toolbar_with_PNG_image.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
