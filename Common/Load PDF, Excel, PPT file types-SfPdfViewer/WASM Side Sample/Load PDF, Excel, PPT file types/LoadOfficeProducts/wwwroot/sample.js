a577a125604dbd8299175461dcc84617b418b96f
Bavatharani-SF3800 <bavatharani.m@syncfusion.com>
2023-06-19 14:57:17 +0530

    UG_Documentation(ES-834513-How-To): Common-Load PDF, Excel, PPT file types- wasm

---
+++ Common/Load PDF, Excel, PPT file types-SfPdfViewer/WASM Side Sample/Load PDF, Excel, PPT file types/LoadOfficeProducts/wwwroot/sample.js
@@ -0,0 +1,16 @@
+﻿var dotnetobject = null;
+window.updateDocument = (data, dotNetinstance) => { 
+    let post = JSON.stringify({
+        'data': data.base64,
+        'type': data.type
+    })
+    dotnetobject = dotNetinstance;
+    const url = "https://localhost:44327/pdfviewer/GetImageStream"
+    let xhr = new XMLHttpRequest()
+    xhr.open('Post', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    xhr.send(post);
+    xhr.onload = function (args) {
+        dotnetobject.invokeMethodAsync('loadPDFdocument', this.responseText) ;
+    }
+  }; 
