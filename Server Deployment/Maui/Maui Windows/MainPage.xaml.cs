31332b4455105775c863a60ab5b29ab8cf500154
SaravanaPriya31 <Saravana31>
2023-07-06 16:01:21 +0530

    commit

---
+++ Server Deployment/Maui/Maui Windows/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiWindows;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Server Deployment/Maui/Maui Windows/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiWindows;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
