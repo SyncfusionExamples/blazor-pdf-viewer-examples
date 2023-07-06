4ab97f6a0b20e538e23598afdb19b123b8d4ac71
SaravanaPriya31 <Saravana31>
2023-07-06 16:22:02 +0530

    commit

--- Server Deployment/Maui - SfPdfViewer/Maui Android/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiApp_Android;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
--- Server Deployment/Maui - SfPdfViewer/Maui Android/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiApp_Android;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
