e58c60dddfc995ff55668ca00c24f6a921f5c11a
VenkadaSubramanian-Durai <venkadasubramanian.d@syncfusion.com>
2025-01-10 11:10:00 +0530

    933125: How to Print large page document in SfPdfViewer sample added

---
+++ Print/Print Large page document/Program.cs
@@ -0,0 +1,33 @@
+using Print_Large_page_document.Components;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSignalR(o => { o.MaximumReceiveMessageSize = 102400000; });
+
+builder.Services.AddMemoryCache();
+//Add Syncfusion Blazor service to the container.
+builder.Services.AddSyncfusionBlazor();
+var app = builder.Build();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR-LICENSE-KEY");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Print/Print Large page document/README.md
@@ -0,0 +1,10 @@
+# Print large page documents in SfPdfViewer component in Blazor
+This guide demonstrates how to implement a custom printing solution for large PDF documents in the Syncfusion<sup style="font-size:70%">&reg;</sup> Blazor SfPdfViewer component.
+
+Documentation: https://blazor.syncfusion.com/documentation/pdfviewer-2/how-to/print-large-page-document
+
+## Project pre-requisites
+To run this application, you need Visual Studio 2019 or later versions.
+
+## Deploying and running the sample
+To debug while running the application, press F5 or select Debug > Start Debugging. To run the sample without debugging, press Ctrl+F5 or selectDebug > Start Without Debugging.
