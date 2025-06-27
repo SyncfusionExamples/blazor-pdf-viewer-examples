c92de7a769b922327f65820bc410fd1f2dab67eb
DivyaUdhayakumar12 <divya.udhayakumar@syncfusion.com>
2025-06-27 17:33:02 +0530

    965102: Documentation for converting Client Point to Page Point and Page Point to Client Point

---
+++ Common/Coordinate Conversion Between Page and Client Points/ConvertPagePointToClientPoint/wwwroot/index.js
@@ -0,0 +1,28 @@
+﻿//// index.js
+
+window.placeRedDotAt = function (x, y, color) {
+    let dot = document.getElementById('click-dot');
+    if (!dot) {
+        dot = document.createElement('div');
+        dot.id = 'click-dot';
+        dot.style.position = 'absolute';
+        dot.style.width = '10px';
+        dot.style.height = '10px';
+        dot.style.backgroundColor = color;
+        dot.style.borderRadius = '50%';
+        dot.style.zIndex = '9999';
+        document.body.appendChild(dot);
+    }
+    dot.style.backgroundColor = color;
+    dot.style.left = x + 'px';
+    dot.style.top = y + 'px';
+};
+
+window.convertPagePointToClientPoint = function (pagePoint) {
+    const pagediv = document.getElementsByClassName('e-pv-page-div')[0];
+    const rect = pagediv.getBoundingClientRect();
+    const clientX = pagePoint.x + rect.left;
+    const clientY = pagePoint.y + rect.top;
+    window.placeRedDotAt(clientX, clientY, "red");
+   
+};
---
+++ Common/Coordinate Conversion Between Page and Client Points/README.md
@@ -0,0 +1,10 @@
+#  Coordinate Conversion Between Page and Client Points in SfPdfViewer.
+Sample to `convertPagePointToClientPoint` that is to convert document page coordinates to browser viewport coordinates and `convertClientPointToPagePoint` to convert viewport coordinates to document page coordinates
+
+Documentation: https://blazor.syncfusion.com/documentation/pdfviewer-2/how-to/coordinate-conversion-between-page-and-client-points
+
+## Project pre-requisites
+To run this application, you need Visual Studio 2019 or later versions.
+
+## Deploying and running the sample
+To debug while running the application, press F5 or select Debug > Start Debugging. To run the sample without debugging, press Ctrl+F5 or selectDebug > Start Without Debugging.
