a87495500ac8812340480cf0d8cbd1429654ec06
Tamilselvan Durairaj <tamilselvan.durairaj@syncfusion.com>
2026-02-10 03:56:41 +0530

    1008834: Updated the load document sample for Blazor pdfviewer

--- Load and Save/Load custom fonts in PDF document/Load custom fonts in PDF document/LoadCustomFonts/Program.cs
+++ Load and Save/Load custom fonts in PDF document/Load custom fonts in PDF document/LoadCustomFonts/Program.cs
@@ -1,15 +1,12 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using LoadCustomFonts.Components;
 using Syncfusion.Blazor;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSignalR(o => { o.MaximumReceiveMessageSize = 102400000; });
-
-// Add Syncfusion Blazor service to the container.
+builder.Services.AddMemoryCache();
+//Add Syncfusion Blazor service to the container.
 builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
@@ -17,7 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -25,10 +22,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
