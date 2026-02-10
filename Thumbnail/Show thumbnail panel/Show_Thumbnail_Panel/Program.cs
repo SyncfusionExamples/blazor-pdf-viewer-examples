538f2e25f37acdbcf94d95a23308f685da31d563
Tamilselvan Durairaj <tamilselvan.durairaj@syncfusion.com>
2026-02-10 09:57:31 +0530

    1008834: Updated the toolbar sample for Blazor pdfviewer

--- Thumbnail/Show thumbnail panel/Show_Thumbnail_Panel/Program.cs
+++ Thumbnail/Show thumbnail panel/Show_Thumbnail_Panel/Program.cs
@@ -1,16 +1,12 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Show_Thumbnail_Panel.Data;
+using Show_Thumbnail_Panel.Components;
 using Syncfusion.Blazor;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSignalR(o => { o.MaximumReceiveMessageSize = 102400000; });
-builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddMemoryCache();
+//Add Syncfusion Blazor service to the container.
 builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
@@ -18,7 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -26,10 +22,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
--- Toolbar/Annotation Toolbar/Show or hide on loading/Show_or_hide_on_loading/Program.cs
+++ Toolbar/Annotation Toolbar/Show or hide on loading/Show_or_hide_on_loading/Program.cs
@@ -1,24 +1,19 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Show_or_hide_on_loading.Data;
+using Show_or_hide_on_loading.Components;
 using Syncfusion.Blazor;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSignalR(o => { o.MaximumReceiveMessageSize = 102400000; });
-builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddMemoryCache();
+//Add Syncfusion Blazor service to the container.
 builder.Services.AddSyncfusionBlazor();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -26,10 +21,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
